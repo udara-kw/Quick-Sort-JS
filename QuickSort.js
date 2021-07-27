f4087374adb0a68327f2b34629dd89d585be4229
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-07-27 11:33:38 +0530

    Add partitioning function

--- QuickSort.js
+++ QuickSort.js
@@ -1,3 +1,16 @@
+function partition(list, low, high) {
+  i = low - 1;
+  pivot = high;
+  for (j = low; j < list.length; j++) {
+    if (list[j] < list[pivot]) {
+      i = i + 1;
+      [list[i], list[j]] = [list[j], list[i]];
+    }
+  }
+  [list[i + 1], list[pivot]] = [list[pivot], list[i + 1]];
+  return i + 1;
+}
+
 function quickSort(list, low, high) {
   if (low < high) {
     pivot = partition(list, low, high);
@@ -5,3 +18,7 @@ function quickSort(list, low, high) {
     quickSort(list, pivot + 1, high);
   }
 }
+
+testList = [10, 34, 21, -3, 0, 44, 987, 67, -42, 65, 8, 9];
+quickSort(testList, 0, testList.length-1);
+console.log(testList);
